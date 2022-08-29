01aa2c8da8b9cefe9ebabc4ed33cf1be9051637f
Madison-Cox <madison.cox5285@hotmail.com>
2022-08-29 18:12:17 -0500

    Added documentation info

--- movie_api/index.js
+++ movie_api/index.js
@@ -1,75 +1,194 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser'),
+    uuid = require('uuid');
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
+
 app.use(morgan('common'));
 
+let users = [
+    {
+        id: 1,
+        name: 'Josh',
+        favoriteMovies: []
+    },
+    {
+        id: 2,
+        name: 'Savannah',
+        favoriteMovies: ['Interstaller']
+    }
+];
 let movies = [
     {
         title: 'Good Will Hunting',
-        director: 'Gus Van Sant',
-        stars: 'Robin Williams, Matt Damon, Ben Affleck'
+        Director: {
+            Name: 'Gus Van Sant'
+        },
+        stars: 'Robin Williams, Matt Damon, Ben Affleck',
+        Genre: {
+            name:'Drama'
+        }
     },
     {
         title: 'Interstellar',
-        director: 'Christopher Nolan',
-        stars: 'Matthew McConaughey, Anne Hathaway, Jessica Chastain'
+        Director: {
+            Name:'Christopher Nolan',
+        },
+        stars: 'Matthew McConaughey, Anne Hathaway, Jessica Chastain',
+        Genre: {
+            name: 'Adventure, Sci-Fi'
+        }
     },
     {
         title: 'Top Gun',
-        director: 'Tony Scott',
-        stars: 'Tom Cruise, Tim Robbins, Kelly McGillis'
+        Director: {
+            Name: 'Tony Scott'
+        },
+        stars: 'Tom Cruise, Tim Robbins, Kelly McGillis',
+        Genre: {
+            name: 'Action'
+        }
     },
     {
         title: 'Dazed and Confused',
-        director: 'Richard Linklater',
-        stars: 'Jason London, Wiley Wiggins, Matthew McConaughey'
+        Director: {
+            Name: 'Richard Linklater'
+        },
+        stars: 'Jason London, Wiley Wiggins, Matthew McConaughey',
+        Genre: {
+            name: 'Comedy'
+        }
     },
     {
         title: 'Star Wars',
-        director: 'George Lucas',
-        stars: 'Mark hamill, Harrison Ford, Carrie Fisher'
+        Director: {
+            Name: 'George Lucas'
+        },
+        stars: 'Mark hamill, Harrison Ford, Carrie Fisher',
+        Genre: 'Sci-Fi'
     },
     {
         title: 'Hook',
-        director: 'Steven Spielberg',
-        stars: 'Dustin Hoffman, Robin Williams, Julia Roberts'
+        Director: {
+            Name: 'Steven Spielberg'
+        },
+        stars: 'Dustin Hoffman, Robin Williams, Julia Roberts',
+        Genre: 'Adventure'
     },
     {
         title: 'Back to the Future',
-        director: 'Robert Zemeckis',
-        stars: 'Michael J. Fox, Christopher Lloyd, Lea Thompson'
+        Director: {
+            Name: 'Robert Zemeckis'
+        },
+        stars: 'Michael J. Fox, Christopher Lloyd, Lea Thompson',
+        Genre: 'Adventure, Comedy, Sci-Fi'
     },
     {
         title: 'Saving Private Ryan',
-        director: 'Steven Spielberg',
-        stars: 'Tom Hanks, Matt Damon, Tom Sizemore'
+        Director: {
+            Name: 'Steven Spielberg'
+        },
+        stars: 'Tom Hanks, Matt Damon, Tom Sizemore',
+        Genre: 'Action'
     },
     {
         title: 'Jurassic Park',
-        director: 'Steven Spielberg',
-        stars: 'Sam Neill, Laura Dern, Jeff Goldblum'
+        Director: {
+            Name: 'Steven Spielberg'
+        },
+        stars: 'Sam Neill, Laura Dern, Jeff Goldblum',
+        Genre: 'Action, Adventure'
     },
     {
         title: 'GoodFellas',
-        director: 'Martin Scorsese',
-        stars: 'Robert De Niro, Ray Liotta, Joe Pesci'
+        Director: {
+            Name: 'Martin Scorsese'
+        },
+        stars: 'Robert De Niro, Ray Liotta, Joe Pesci',
+        Genre: 'Drama, Biography, Crime'
     }
 ];
 
+//CREATE
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser)
+    } else {
+        res.status(400).send('users need names')
+    }
+})
+//UPDATE
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
 
+    let user = users.find( user => user.id == id);
+
+    if (user) {
+        user.name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send('')
+    }
+})
+
+//CREATE
+app.post('/users/:id/:movietitle', (req, res) => {
+    const { id, movietitle } = req.params;
+
+    let user = users.find( user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies.push(movietitle);
+        res.status(200).send(`${movietitle} has been added to user ${id}'s array`);
+    } else {
+        res.status(400).send('')
+    }
+})
+//DELETE
+app.delete('/users/:id/:movietitle/', (req, res) => {
+    const { id, movietitle } = req.params;
+
+    let user = users.find( user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movietitle);
+        res.status(200).send(`${movietitle} has been removed from user ${id}'s array`);;
+    } else {
+        res.status(400).send('')
+    }
+})
+//DELETE
+app.delete('/users/:id/', (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find( user => user.id == id);
+
+    if (user) {
+        users = users.filter( user => user.id != id);
+        res.status(200).send(`user ${id} has been deleted`);
+    } else {
+        res.status(400).send('')
+    }
+})
+//READ
 app.get('/', (req, res) => {
     res.send('This is MovieScout');
 });
 
-//read
+//READ
 app.get('/movies', (req, res) => {
     res.status(200).json(movies)
 });
 
-//read
+//READ
 app.get('/movies/:title', (req, res) => {
     const { title } = req.params;
     const movie = movies.find(movie => movie.title === title);
@@ -81,9 +200,28 @@ app.get('/movies/:title', (req, res) => {
     }
 });
 
-app.use((err, req, res, next) => {
-    console.log(err.stack);
-    res.status(500).send('Something Broke');
+//READ
+app.get('/movies/Genre/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = movies.find(movie => movie.Genre.name === genreName).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('Genre not found')
+    }
+});
+
+//READ
+app.get('/movies/director/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find (movie => movie.Director.Name === directorName).Director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('Director not found')
+    }
 });
 
 app.listen(5285, () => {
