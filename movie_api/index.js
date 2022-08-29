988fe18e7e84192f85b40f7dff110afb44fe2b38
Madison-Cox <madison.cox5285@hotmail.com>
2022-08-29 09:08:06 -0500

    added morgan, top movies,

--- movie_api/index.js
+++ movie_api/index.js
@@ -1,2 +1,77 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(express.static('public'));
+app.use(morgan('common'));
+
+let topMovies = [
+    {
+        title: 'Good Will Hunting',
+        director: 'Gus Van Sant',
+        stars: 'Robin Williams, Matt Damon, Ben Affleck'
+    },
+    {
+        title: 'Interstellar',
+        director: 'Christopher Nolan',
+        stars: 'Matthew McConaughey, Anne Hathaway, Jessica Chastain'
+    },
+    {
+        title: 'Top Gun',
+        director: 'Tony Scott',
+        stars: 'Tom Cruise, Tim Robbins, Kelly McGillis'
+    },
+    {
+        title: 'Dazed and Confused',
+        director: 'Richard Linklater',
+        stars: 'Jason London, Wiley Wiggins, Matthew McConaughey'
+    },
+    {
+        title: 'Star Wars',
+        director: 'George Lucas',
+        stars: 'Mark hamill, Harrison Ford, Carrie Fisher'
+    },
+    {
+        title: 'Hook',
+        director: 'Steven Spielberg',
+        stars: 'Dustin Hoffman, Robin Williams, Julia Roberts'
+    },
+    {
+        title: 'Back to the Future',
+        director: 'Robert Zemeckis',
+        stars: 'Michael J. Fox, Christopher Lloyd, Lea Thompson'
+    },
+    {
+        title: 'Saving Private Ryan',
+        director: 'Steven Spielberg',
+        stars: 'Tom Hanks, Matt Damon, Tom Sizemore'
+    },
+    {
+        title: 'Jurassic Park',
+        director: 'Steven Spielberg',
+        stars: 'Sam Neill, Laura Dern, Jeff Goldblum'
+    },
+    {
+        title: 'GoodFellas',
+        director: 'Martin Scorsese',
+        stars: 'Robert De Niro, Ray Liotta, Joe Pesci'
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('This is MovieScout');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send('Something Broke');
+});
+
+app.listen(5285, () => {
+    console.log('listening on port 5285');
+});
