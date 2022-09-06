f9d8edef0084d756488ad5e497097aeb9a5d6daf
Madison-Cox <madison.cox5285@hotmail.com>
2022-09-06 13:53:35 -0500

    fixed typo

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const cors = require('cors');
 
 app.use(cors({
     origin: (origin, callback) => {
-        if(!origin) return callabck(null, true);
+        if(!origin) return callback(null, true);
         if(allowedOrigins.indexOf(origin) === -1){
             let message = 'The CORS policy for this application does not allow access from origin ' + origin;
             return callback(new Error(message ), false);
