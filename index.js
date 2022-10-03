e20979e1362689606c4467a2a732e648adb0b47a
Madison-Cox <madison.cox5285@hotmail.com>
2022-10-03 08:11:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/users',
 //CREATE ADD A MOVIE TO USERS FAVORITES
 app.post('/users/:Username/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOneAndUpdate({ Username: req.params.Username }, {
-        $push: { FavoriteMovies: req.params.MovieTitle }
+        $push: { FavoriteMovies: req.params.Title }
     },
         { new: true },
         (err, UpdatedUser) => {
